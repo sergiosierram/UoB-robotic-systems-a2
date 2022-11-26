20ede710c35ecba647d8420c6a33b42ffae56169
Sergio Sierra <sergio.sierra@imexhs.com>
2022-11-26 18:24:11 +0000

    First version odometry

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,6 @@
 #include "motors.h"
 #include "linesensor.h"
-//#include "encoders.h"
+#include "encoders.h"
 #include "kinematics.h"
 #include "pid.h"
 
@@ -20,6 +20,7 @@ boolean led_state;  // Variable to "remember" the state
 
 Motors_c motors;
 LineSensor_c lsensors;
+Kinematics_c kinematics;
 
 int state = 0;
 
@@ -55,104 +56,26 @@ void setup() {
   lsensors.initialise();
   lsensors.calibrateSensors();
 
+  //Initialise encoders
+  setupEncoder0();
+  setupEncoder1();
+
   unsigned long tick = millis();
 
-  
 }
 
 
 // put your main code here, to run repeatedly:
 void loop() {
-  
-  
-  if (state == STATE_START){
-    motors.setMotorPower(30,30);
-    if (lsensors.onBlack()){
-      motors.setMotorPower(0,0);
-      state++;
-    }  
-  } 
-  if (state == STATE_LINE_REACHED){
-    motors.setMotorPower(0,30);
-    state++;
+  unsigned long tock = millis();
+  unsigned long dt = tock - tick ;
+  if (dt > 50){
+    kinematics.update(-1*count_e1, -1*count_e0, dt/1000.0);
+    tick = millis();
   }
-  if (state == STATE_JOINING){
-    if (lsensors.onLine()){
-      motors.setMotorPower(0,0);
-      state++;
-    }
-  }
-  if (state == STATE_ONLINE){
-    eline = lsensors.getLineError(true);
-
-    if (eline > -2 and eline < 2 ){    
-      d_eline = eline - last_eline;
-      i_eline = eline + last_eline;
-      control_action = 1.5*eline + 0.5*d_eline + 0.1*i_eline;
-      int vl = 30 - 30*control_action;
-      int vr = 30 + 30*control_action;
-      motors.setMotorPower(vl,vr);
-    } else if (eline == 2){
-      motors.setMotorPower(0,30);
-      delay(50);
-    } else if (eline == -2){
-      motors.setMotorPower(30,0);
-      delay(50);
-    } else if (eline == 10){
-      motors.setMotorPower(-30,-30);
-      delay(50);
-    } 
-    
-    last_eline = eline;
-
-    //New
-    white = lsensors.onWhite();
-    if (white and !last_white){
-      tick = millis();
-      if (tick < 6000){
-        reverse = 1;
-        motors.setMotorPower(-30,30);
-        delay(1200);
-      } else {
-        last_white = true;
-      }
-    } else if (white and last_white){
-      tock = millis() - tick;
-      Serial.println(tock);
-      if (tock > 1500){
-        motors.setMotorPower(0,0);
-        state++;
-      }  
-    }
-    last_white = white;
-  }
-  //New
-  if (state == STATE_LINE_END){
-    motors.setMotorPower(-30,30);
-    delay(1330);
-    motors.setMotorPower(0,0);
-    delay(50);
-    motors.setMotorPower(30,30);
-    delay(2000);
-    motors.setMotorPower(0,0);
-    delay(50);
-    if (reverse == 0){
-      motors.setMotorPower(-30,30);
-    } else {
-      motors.setMotorPower(30,-30);
-    }
-    delay(1330/2);
-    motors.setMotorPower(0,0);
-    delay(50);
-    motors.setMotorPower(30,30);
-    delay(12000);
-    motors.setMotorPower(0,0);
-    state++;
-  }
-    
-  //lsensors.getLineError(true);
-  lsensors.readLineSensorsCalibrated(true);
-  
-  delay(50);
-
+//  Serial.print(-1*count_e1);
+//  Serial.print(",");
+//  Serial.println(-1*count_e0);
+//  
+  delay(1);
 }
--- arduino/kinematics.h
+++ arduino/kinematics.h
@@ -4,6 +4,27 @@
 #ifndef _KINEMATICS_H
 #define _KINEMATICS_H
 
+// Define global variables here
+volatile long count_e1_prev = 0;
+volatile long count_e0_prev = 0;
+
+float x_local = 0; // x coordinate of robot in the local frame
+float y_local = 0; // y coordinate of robot in the local frame
+float theta_local = 0; // angular position of robot in the global frame
+
+
+float encoder_count = 358.3;
+
+float l = 0.0446; // 
+float r = 0.016; //
+
+float phi_left = 0; // angular velocity of the left wheel;
+float phi_right = 0; // angular velocity of the right wheel;
+
+float x_global = 0; // x coordinate of robot in the global frame
+float y_global = 0; // y coordinate of robot in the global frame
+float theta_global = 0; // angular position of robot in the global frame
+
 // Class to track robot position.
 class Kinematics_c {
   public:
@@ -15,11 +36,57 @@ class Kinematics_c {
 
     // Use this function to update
     // your kinematics
-    void update() {
+    void update(volatile long count_e1, volatile long count_e0, float dt) {
+      
+      angularVelocity(count_e1,count_e0, dt);
 
+      float u = 0.5*(phi_left + phi_right);
+      float w = (phi_right - phi_left)/(2*l);
+
+      x_local = u*cos(theta_local);
+      y_local = u*sin(theta_local);
+      
+      x_global = x_global + x_local*dt;
+      y_global = y_global + y_local*dt;
+      theta_local = theta_local + dt*w;
+      
+      
+
+      Serial.println(x_global);
     }
 
+    void angularVelocity(volatile long count_e1, volatile long count_e0, float dt) {
+      // This function calculates the angular velocity of each wheel
+      phi_left = (r/dt) * (((count_e1-count_e1_prev)/358.3)*2*PI);
+      phi_right = (r/dt) * (((count_e0-count_e0_prev)/358.3)*2*PI);
+      count_e1_prev = count_e1;
+      count_e0_prev = count_e0;
+    }
+
+   
+
+
+   
 };
+
+
+//
+//
+//float x_local = 0; // x coordinate of robot in the local frame
+//float y_local = 0; // y coordinate of robot in the local frame
+//float theta_local = 0; // angular position of robot in the global frame
+//
+//
+//
+
+//
+//void distanceGlobal() {
+//  // This function helps the robot to move within the global frame
+//  x_global = x_global + (x_local*cos(theta_global));
+//  y_global = y_global + (x_local*sin(theta_global));
+//  theta_global = theta_global + theta_local;
+//}
+//
 
 
 
