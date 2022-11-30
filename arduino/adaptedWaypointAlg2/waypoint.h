6b734d1724abdb378e7096166731045ee8c50c31
Audric <atamby187@outlook.com>
2022-11-30 19:51:34 +0000

    Added new files as arduino in my computer is not working

--- arduino/adaptedWaypointAlg2/waypoint.h
+++ arduino/adaptedWaypointAlg2/waypoint.h
@@ -14,9 +14,13 @@ class Waypoint_c {
      * x_path and y_path are the arrays to save the points
     */
     int16_t idx = 0;
+    int16_t count_skip = 0;
     int16_t t = 0;
     int16_t x_path[100];
+    int16_t x_path_adapt[100];
     int16_t y_path[100];
+    int16_t y_path_adapt[100];
+    float theta_path[100];
   
     // Constructor, must exist.
     /*
@@ -26,7 +30,8 @@ class Waypoint_c {
     Waypoint_c() {
       for (int16_t i = 0; i < 100; i ++){
         x_path[i] = 0;
-        y_path[i] = 0;  
+        y_path[i] = 0;
+        theta_path[i] = 0;  
       }
 
     }
@@ -38,10 +43,11 @@ class Waypoint_c {
      * Idx variable keeps track of the current position of the array, where the 
      * next point needs to be saved.
     */
-    void savePoint(int16_t x, int16_t y){
+    void savePoint(int16_t x, int16_t y, float theta){
       if (idx < 100){
         x_path[idx] = x;
         y_path[idx] = y;
+        theta_path[idx] = theta;
         idx++;
       } else {
         Serial.println("Skipping");
@@ -54,10 +60,27 @@ class Waypoint_c {
     */
     void printPoints(){
       for (int16_t i = 0; i < idx; i++ ){
-        Serial.print(x_path[i]);
+        Serial.print(x_path_adapt[i]);
         Serial.print(", ");
-        Serial.println(y_path[i]);
+        Serial.print(y_path_adapt[i]);
+        Serial.print(", ");
+        Serial.println(theta_path[i]);
+
+
       }  
+    }
+
+    // This function reduces the number of waypoints in the path
+    void adaptiveWP(){
+      // Store the first waypoint as point of reference
+      x_path_adapt[0] = x_path[0];
+      y_path_adapt[0] = y_path[0];
+      for (int16_t i = 1; i < idx; i++){
+        if (abs(theta_path[i]-theta_path[i-1]) > 0.04){ // if the robot is not on a straight, keep the waypoint
+          x_path_adapt[i] = x_path[i];
+          y_path_adapt[i] = y_path[i];
+        } 
+      }
     }
 
 };
